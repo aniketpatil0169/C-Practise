36e6e3b6af2ee0700f9cba433b2dd311c8df29cc
aniketpatil0169 <148536939+aniketpatil0169@users.noreply.github.com>
2024-03-03 22:21:02 +0530

    Create Array.cs

---
+++ Array.cs
@@ -0,0 +1,90 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region array example
+            //// comma separated input to find even numbers
+
+            //Console.WriteLine("Please enter comma separated numbers");
+            //string input = Console.ReadLine();
+
+            //string[] numbers = input.Split(',');
+
+            //int counter = 0;
+            //for (int i = 0; i < numbers.Length; i++)
+            //{
+            //    if (int.Parse(numbers[i]) % 2 == 0)
+            //    {
+            //        Console.Write($"{numbers[i]} ");
+            //        counter++;
+            //    }
+            //}
+            //Console.WriteLine($"\nNumber of Even Numbers : {counter}");
+
+            #endregion array example
+
+            #region even number till given target
+
+            //Console.WriteLine("Please enter a target number");
+            //int target = int.Parse(Console.ReadLine());
+
+            //int evenCount = target / 2;
+            //int[] evens = new int[evenCount];
+            //// int counter = 2;
+
+            //for (int i = 0, counter = 2; i < evenCount; i++, counter+=2)
+            //{
+            //    evens[i] = counter;
+            //    //if(i % 2 == 0)
+            //    //{
+            //    //    evens[i] = i;
+            //    //}
+            //}
+
+            //for (int i = 0; i < evens.Length; i++)
+            //{
+            //    Console.Write($"{evens[i]} ");
+            //}
+
+            ////for (int i = 1; i <= target; i++)
+            ////{
+            ////    if(i % 2 == 0)
+            ////    {
+            ////        Console.Write($"{i} ");
+            ////    }
+            ////}
+            //Console.WriteLine(); // new line
+
+            #endregion even number till given target
+
+            int[,] numbers = new int[3, 3];
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    
+                    if (i == j)
+                    {
+                        Console.Write($"*\t");
+                    }
+                    else
+                    {
+                        Console.Write($"\t");
+                    }
+                }
+                Console.WriteLine(); // new line
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
