534a663445f945d7d5d669adb092d8eb6bbb0d1a
aniketpatil0169 <148536939+aniketpatil0169@users.noreply.github.com>
2024-03-15 14:37:51 +0530

    Create ClassDemoCircle.cs

---
+++ ClassDemoCircle.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassDemoCircle
+{
+    internal class Circle
+    {
+        public int Radius; // instance member
+        public static float Pi = 3.14f; // static member
+
+        public void Area() // instance member
+        {
+            Console.WriteLine($"Area Of Circle = {Pi * Radius * Radius}");
+        }
+    }
+}
